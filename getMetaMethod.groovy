f9538cb58743c0cc235e6a87898f0cefa9410e81
quguocheng <quguocheng@meituan.com>
2015-12-16 17:34:39 +0800

    renamed:    sample.groovy -> getMetaMethod.groovy

--- getMetaMethod.groovy
+++ getMetaMethod.groovy
@@ -41,10 +41,7 @@ println method
 
 
 /*
-注意下面两种调用info的方式。
-第一种方式出现了wrong number of arguments异常
-第二种方式正常work
-猜测：第一种方式，groovy将“call logger”作为了Object[]的第一个元素，认为没有传递String参数
+出现了wrong number of arguments异常
 */
 method = Logger.metaClass.getMetaMethod("info",["",null,2] as Object[])
 println method
@@ -53,12 +50,3 @@ try{
 }catch(IllegalArgumentException e){
     println "method.invoke(logger,['call logger', null, 2]as Object[]) failed:${e}"
 }
-method.invoke(logger, "call logger", [1,2]as Object[])
-
-
-def args=["heihei",null,2] as Object[]
-method = Logger.metaClass.getMetaMethod("info", args)
-println ""
-println method
-method.invoke(logger, (String)args[0], (Object[])args[1,args.length-1])
-
